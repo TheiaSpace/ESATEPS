7b9cf942327f062a277712293e26d51f0a48d255
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2017-11-28 16:28:19 +0100

    Bump version number to 2.0.1

--- src/ESAT_EPS.h
+++ src/ESAT_EPS.h
@@ -92,7 +92,7 @@ class ESAT_EPSClass
     // Software version number.
     static const byte MAJOR_VERSION_NUMBER = 2;
     static const byte MINOR_VERSION_NUMBER = 0;
-    static const byte PATCH_VERSION_NUMBER = 0;
+    static const byte PATCH_VERSION_NUMBER = 1;
 
     // Set current time command size:
     // - Year (2 byte).
