871d1c9110cf3e27e368ee800c86e9c34ba01bab
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-10-24 11:29:21 +0200

    Update the copyright years of the main program.

--- examples/EPS/EPS.ino
+++ examples/EPS/EPS.ino
@@ -1,6 +1,6 @@
 /*
  * ESAT EPS Main Program version 2.1.0
- * Copyright (C) 2017 Theia Space, Universidad Politécnica de Madrid
+ * Copyright (C) 2017, 2018 Theia Space, Universidad Politécnica de Madrid
  *
  * This file is part of Theia Space's ESAT EPS library.
  *
