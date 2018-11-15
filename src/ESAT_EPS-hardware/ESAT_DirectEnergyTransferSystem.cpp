47a86b33f473a898b65e77f5cce1b99063f05974
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-11-15 11:27:24 +0100

    Document the whys of ESAT_DirectEnergyTransferSystem.cpp

--- src/ESAT_EPS-hardware/ESAT_DirectEnergyTransferSystem.cpp
+++ src/ESAT_EPS-hardware/ESAT_DirectEnergyTransferSystem.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Theia Space, Universidad Politécnica de Madrid
+ * Copyright (C) 2017, 2018 Theia Space, Universidad Politécnica de Madrid
  *
  * This file is part of Theia Space's ESAT EPS library.
  *
@@ -23,6 +23,12 @@
 
 word ESAT_DirectEnergyTransferSystemClass::read(const word channelConfigurationBits)
 {
+  // The reading/measurement process consists of two steps:
+  // 1) command the measurement device to perform an analog-to-digital
+  // conversion;
+  // 2) ask the measurement device for the reading.
+  // The measurement device has several channel; to select the desired
+  // channel, we just pass it a set of channel configuration bits.
   const boolean successfulConversionStart =
     startConversion(channelConfigurationBits);
   if (!successfulConversionStart)
@@ -30,6 +36,8 @@ word ESAT_DirectEnergyTransferSystemClass::read(const word channelConfigurationB
     error = true;
     return 0;
   }
+  // The measurement device needs some time to have the measurement
+  // ready.  1 ms is enough for this.
   delay(1);
   const word convertedValue = readConvertedValue();
   return convertedValue;
@@ -37,6 +45,9 @@ word ESAT_DirectEnergyTransferSystemClass::read(const word channelConfigurationB
 
 word ESAT_DirectEnergyTransferSystemClass::readConvertedValue()
 {
+  // The reading comes from an I2C device.  It is encoded as an
+  // uncalibrated 16-bit unsigned integer in big-endian format.  We
+  // leave the calibration step to the ground segment.
   WireEPS.beginTransmission(ADDRESS);
   WireEPS.write(CONVERSION_REGISTER);
   const byte writeStatus = WireEPS.endTransmission();
@@ -73,6 +84,11 @@ word ESAT_DirectEnergyTransferSystemClass::readVoltage()
 
 boolean ESAT_DirectEnergyTransferSystemClass::startConversion(const word channelConfigurationBits)
 {
+  // To take a measurement, we have to command the measurement device
+  // to start an analog-to-digital conversion.  The command goes
+  // through the I2C bus and consists of a set of configuration bits;
+  // some of them are common to all measurement channels and others
+  // are channel-specific.
   const word configurationBits =
     START_SINGLE_SHOT_CONVERSION_CONFIGURATION_BITS |
     COMPARATOR_CONFIGURATION_BITS |
